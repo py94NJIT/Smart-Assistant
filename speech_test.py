77c11354a855bdf834cfdaf795792a75246fb900
py94NJIT <py94@njit.edu>
2025-12-12 21:27:54 -0500

    Initial command for weather api integration

--- speech_test.py
+++ speech_test.py
@@ -1,13 +1,22 @@
 
 import speech_recognition as sr
 import pyttsx3
-
+import requests
 
 # Initialize recognizer class (for recognizing speech)
 r= sr.Recognizer()
 
 #Initialize TTS engine
 engine = pyttsx3.init()
+
+#Weather API Key and Base URL
+API_KEY = "YourKey"
+#Url Format after timeline/location/date/key
+weather_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/2020-10-01/2020-12-31?key=YOURAPIKEY"
+
+# Initialize requests
+request = requests.get(weather_url)
+weather_data = request.json()
 
 #Speak
 def speak(text):
@@ -35,12 +44,18 @@ def listen():
         except sr.RequestError:
             speak(f"Could not request results from speech recognition service;")
             return ""
+        
+#Processing User commands, and responding appropriately
 def process_command(command):
     if "hello" in command:
         speak("Hello there!")
     elif "quit" in command:
         speak("Goodbye!")
         return True
+    elif "weather" in command:
+        # Location based on current user location
+        address = weather_data['address']
+        speak("The current weather for {weather_data['address']} is {weather_data['conditions']}]")
     else:
         speak("I'm sorry, I don't know how to do that yet.")
     return False
